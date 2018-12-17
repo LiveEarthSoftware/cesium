272ab5dd65eb7c751298549f044d928cf09e2796
Zac <zburns@liveearthsoftware.com>
2018-12-17 11:06:10 -0600

    depthless pass fixes

--- Source/Scene/Scene.js
+++ Source/Scene/Scene.js
@@ -801,6 +801,10 @@ define([
         this._removeTaskProcessorListenerCallback = TaskProcessor.taskCompletedEvent.addEventListener(requestRenderAfterFrame(this));
         this._removeGlobeCallbacks = [];
 
+        // <LIVE_EARTH>
+        this.depthlessPass = new Event();
+        // </LIVE_EARTH>
+
         // initial guess at frustums.
         var near = camera.frustum.near;
         var far = camera.frustum.far;
@@ -2521,31 +2525,6 @@ define([
 
             passState.framebuffer = originalFramebuffer;
         }
-
-        // <LIVE_EARTH>
-        // Allow direct rendering of drawing groups outside of frustum/pass system
-
-        // This is copied from above at the beginning of the function, just resetting
-        // the frustum to include the whole scene instead of using the nearest which
-        // may clip.
-        if (defined(camera.frustum.fov)) {
-            frustum = camera.frustum.clone(scratchPerspectiveFrustum);
-        } else if (defined(camera.frustum.infiniteProjectionMatrix)){
-            frustum = camera.frustum.clone(scratchPerspectiveOffCenterFrustum);
-        } else if (defined(camera.frustum.width)) {
-            frustum = camera.frustum.clone(scratchOrthographicFrustum);
-        } else {
-            frustum = camera.frustum.clone(scratchOrthographicOffCenterFrustum);
-        }
-
-        // Ideally, we would render the sky box and atmosphere last for
-        // early-z, but we would have to draw it in each frustum
-        frustum.near = camera.frustum.near;
-        frustum.far = camera.frustum.far;
-        us.updateFrustum(frustum);
-
-        Scene.executeCommandsLate(scene, passState);
-        // </LIVE_EARTH>
     }
 
     function executeComputeCommands(scene) {
@@ -3130,6 +3109,15 @@ define([
             passState.framebuffer = usePostProcess ? sceneFramebuffer : defaultFramebuffer;
             scene._oit.execute(context, passState);
         }
+
+        // <LIVE_EARTH>
+        // We have to execute after the OIT composite pass, but before postProcess
+        // otherwise drawing groups render under translucent objects.
+        // In order to resolve that so the composite worked, we would have
+        // to render depth in each frustum.
+        // <LIVE_EARTH>
+        scene.depthlessPass.raiseEvent(scene, passState);
+        // </LIVE_EARTH>
 
         if (usePostProcess) {
             var inputFramebuffer = sceneFramebuffer;
@@ -4004,10 +3992,6 @@ define([
 
         return destroyObject(this);
     };
-
-    // <LIVE_EARTH>
-   Scene.executeCommandsLate = function(scene, passState) {};
-   // </LIVE_EARTH>
 
     return Scene;
 });
